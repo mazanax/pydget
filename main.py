9d139866228e852ea4e4dfd51ee4367412a86988
Alexander <agorshkov@mznx.ru>
2022-05-17 11:25:39 +0300

    Remove unused code

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ from PIL import ImageTk, Image
 from pic2str import explode
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-
 class FloatingWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
